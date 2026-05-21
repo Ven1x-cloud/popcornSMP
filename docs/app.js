7dd92654077d6f47da6a892c933996a06ba433bf
Ven1x-cloud <126881@leerling.delagewaard.nl>
2026-05-21 10:56:16 +0200

    Change API base URL to production endpoint

--- docs/app.js
+++ docs/app.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:5000";
+const API_BASE = "https://v1rprogram.pythonanywhere.com";
 const tokenKey = "smp_token";
 
 const meOut = document.getElementById("meOut");
